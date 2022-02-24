ef4100e13a9e15c7b4a9f2299af5d6291b3c963d
ffff19 <fprogramming@protonmail.com>
2022-02-24 08:03:15 -0300

    .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	if (num < 0)
+		return "ERROR";
+	let finalString = '';
+	while (num > 0) {
+		finalString += string;
+		num--;
+	}
+	return finalString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+		return "ERROR";
 
+	let sum = 0;
+	if (a < b) {
+		for (; a <= b; a++) {
+			sum += a;
+		}
+	} else
+		for (; b <= a; b++) {
+			sum += b;
+		}
+	return sum;
 };
 
 // Do not edit below this line
