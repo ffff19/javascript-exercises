7abdb14231f2d4e652ca0becc79045d0f835684b
ffff19 <fprogramming@protonmail.com>
2022-02-25 09:24:54 -0300

    .

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+	let oldestIndex = 0;
+	let oldestAge;
+	let currentYear = Date().slice(11, 15);
+	if (array[0].yearOfDeath != undefined) {
+		oldestAge =	array[0].yearOfDeath - array[0].yearOfBirth;
+	} else {
+		oldestAge = currentYear - array[0].yearOfBirth; 
+	}
+	for (let i = 1; i < array.length; i++) {
+		if (array[i].yearOfDeath != undefined) {
+			if (array[i].yearOfDeath - array[i].yearOfBirth > oldestAge) {
+				oldestIndex = i;
+				oldestAge = array[i].yearOfDeath - array[i].yearOfBirth;
+			}
+		} else {
+			if (currentYear - array[i].yearOfBirth > oldestAge) {
+				oldestIndex = i;
+				oldestAge = currentYear - array[i].yearOfBirth;
+			}
+		}
+	}
+	return array[oldestIndex];
 };
 
 // Do not edit below this line
